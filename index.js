7bf95e452bf812830c9079d25f88d5ce093d1b88
Bjørn Sandvik <bjorn@mastermaps.com>
2017-06-29 16:16:46 +0200

    Added chart

--- index.js
+++ index.js
@@ -1,164 +1,162 @@
-var features;
-var data;
-var chartData = {};
-var years = [];
-var lastYear;
+$(function() {
 
-var colors = {
-  p: '#a8ddb5',
-  s: '#43a2ca'
-};
+  var features;
+  var data;
+  var chartData = {};
+  var years = [];
+  var lastYear;
 
-var bounds = [[-40, -110], [60, 165]];
-var map = L.map('map', {
-  zoomSnap: 0.2,
-  zoomDelta: 1,
-  attributionControl: false,
-  scrollWheelZoom: false
-});
+  var colors = {
+    p: '#a8ddb5',
+    s: '#43a2ca'
+  };
 
-map.fitBounds(bounds);
+  var bounds = [[-40, -110], [60, 165]];
+  var map = L.map('map', {
+    zoomSnap: 0.2,
+    zoomDelta: 1,
+    attributionControl: false,
+    scrollWheelZoom: false
+  });
 
-$.getJSON('countries_indian_states.json', function(data) {
-  features = L.geoJSON(data, {
-    color: '#555',
-    weight: 1,
-    fillColor: '#eee',
-    fillOpacity: 1
-  }).addTo(map);
-  onDataLoad();
-});
+  map.fitBounds(bounds);
 
-/*
-L.rectangle(bounds, {
-  fill: false
-}).addTo(map);
-*/
+  $.getJSON('countries_indian_states.json', function(data) {
+    features = L.geoJSON(data, {
+      color: '#555',
+      weight: 1,
+      fillColor: '#eee',
+      fillOpacity: 1
+    }).addTo(map);
+    onDataLoad();
+  });
 
-// https://coderwall.com/p/duapqq/use-a-google-spreadsheet-as-your-json-backend
-// https://spreadsheets.google.com/feeds/list/0AtMEoZDi5-pedElCS1lrVnp0Yk1vbFdPaUlOc3F3a2c/od6/public/values?alt=json-in-script&callback=x
-// https://stackoverflow.com/questions/24531351/retrieve-google-spreadsheet-worksheet-json
-$.getJSON('//spreadsheets.google.com/feeds/list/1Fd-vBoJPjp5wdCyJc7d_LOJPOg5uqdzVa3Eq5-VFR-g/1/public/values?alt=json-in-script&callback=?', function(sheet) {
-  var rows = sheet.feed.entry;
-  var row = rows[0];
+  // https://coderwall.com/p/duapqq/use-a-google-spreadsheet-as-your-json-backend
+  // https://spreadsheets.google.com/feeds/list/0AtMEoZDi5-pedElCS1lrVnp0Yk1vbFdPaUlOc3F3a2c/od6/public/values?alt=json-in-script&callback=x
+  // https://stackoverflow.com/questions/24531351/retrieve-google-spreadsheet-worksheet-json
+  $.getJSON('//spreadsheets.google.com/feeds/list/1Fd-vBoJPjp5wdCyJc7d_LOJPOg5uqdzVa3Eq5-VFR-g/1/public/values?alt=json-in-script&callback=?', function(sheet) {
+    var rows = sheet.feed.entry;
+    var row = rows[0];
 
-  for (var col in row) {
-    if (row.hasOwnProperty(col)) {
-      if (col.slice(0, 5) === 'gsx$y') {
-        years.push(col.slice(-4));
+    for (var col in row) {
+      if (row.hasOwnProperty(col)) {
+        if (col.slice(0, 5) === 'gsx$y') {
+          years.push(col.slice(-4));
+        }
       }
+    }
+
+    lastYear = years[years.length - 1];
+    data = {};
+
+    rows.forEach(function(row) {
+      var id = row['gsx$code']['$t'];
+      var name = row['gsx$name']['$t'];
+      var value =
+
+      data[id] = {
+        name: name
+      };
+
+      years.forEach(function(year) {
+        var value = row['gsx$y' + year]['$t'];
+
+        if (value) {
+          data[id][year] = row['gsx$y' + year]['$t'] || null;
+
+          if (!chartData[year]) {
+            chartData[year] = {
+              p: 0,
+              s: 0,
+            }
+          }
+
+          chartData[year][value]++;
+        }
+      });
+    });
+
+    onDataLoad();
+  });
+
+  function onDataLoad() {
+    if (features && data) {
+      createChart();
+
+      features.eachLayer(function(item) {
+
+        var code = item.feature.properties.ISO_A2;
+
+        if (code === 'IN') {
+          code = 'IN-' + item.feature.properties.POSTAL;
+        }
+
+        if (code && data[code] && data[code][lastYear]) {
+          var value = data[code][lastYear];
+
+          item.setStyle({
+            fillColor: colors[value]
+          });
+
+          item.bindPopup('<strong>' + data[code].name + '</strong><br/>' + ((value === 's') ? 'National rollout' : 'Programs/partial'));
+        }
+      });
     }
   }
 
-  lastYear = years[years.length - 1];
-  data = {};
 
-  rows.forEach(function(row) {
-    var id = row['gsx$code']['$t'];
-    var name = row['gsx$name']['$t'];
-    var value =
-
-    data[id] = {
-      name: name
-    };
+  function createChart() {
+    var pilotByYear = [];
+    var nationalByYear = [];
 
     years.forEach(function(year) {
-      var value = row['gsx$y' + year]['$t'];
+      pilotByYear.push(chartData[year].p);
+      nationalByYear.push(chartData[year].s);
+    });
 
-      if (value) {
-        data[id][year] = row['gsx$y' + year]['$t'] || null;
-
-        if (!chartData[year]) {
-          chartData[year] = {
-            p: 0,
-            s: 0,
+    Highcharts.chart('chart', {
+      chart: {
+        type: 'area'
+      },
+      title: {
+        text: 'Countries using DHIS 2'
+      },
+      xAxis: {
+        categories: years,
+        tickmarkPlacement: 'on',
+        title: {
+          enabled: false
+        }
+      },
+      yAxis: {
+        title: {
+          text: 'Number of countries'
+        }
+      },
+      tooltip: {
+        split: true,
+      },
+      plotOptions: {
+        area: {
+          stacking: 'normal',
+          lineColor: '#666666',
+          lineWidth: 1,
+          marker: {
+            lineWidth: 1,
+            lineColor: '#666666'
           }
         }
-
-        chartData[year][value]++;
-      }
-    });
-  });
-
-  onDataLoad();
-});
-
-function onDataLoad() {
-  if (features && data) {
-    createChart();
-
-    features.eachLayer(function(item) {
-
-      var code = item.feature.properties.ISO_A2;
-
-      if (code === 'IN') {
-        code = 'IN-' + item.feature.properties.POSTAL;
-      }
-
-      if (code && data[code] && data[code][lastYear]) {
-        var value = data[code][lastYear];
-
-        item.setStyle({
-          fillColor: colors[value]
-        });
-
-        item.bindPopup('<strong>' + data[code].name + '</strong><br/>' + ((value === 's') ? 'National rollout' : 'Programs/partial'));
-      }
+      },
+      series: [{
+        name: 'Pilot',
+        data: pilotByYear,
+        color: colors.p
+      }, {
+        name: 'National scale',
+        data: nationalByYear,
+        color: colors.s
+      }]
     });
   }
-}
 
-
-function createChart() {
-  var pilotByYear = [];
-  var nationalByYear = [];
-
-  years.forEach(function(year) {
-    pilotByYear.push(chartData[year].p);
-    nationalByYear.push(chartData[year].s);
-  });
-
-  Highcharts.chart('chart', {
-    chart: {
-      type: 'area'
-    },
-    title: {
-      text: 'Countries using DHIS 2'
-    },
-    xAxis: {
-      categories: years,
-      tickmarkPlacement: 'on',
-      title: {
-        enabled: false
-      }
-    },
-    yAxis: {
-      title: {
-        text: 'Number of countries'
-      }
-    },
-    tooltip: {
-      split: true,
-    },
-    plotOptions: {
-      area: {
-        stacking: 'normal',
-        lineColor: '#666666',
-        lineWidth: 1,
-        marker: {
-          lineWidth: 1,
-          lineColor: '#666666'
-        }
-      }
-    },
-    series: [{
-      name: 'Pilot',
-      data: pilotByYear,
-      color: colors.p
-    }, {
-      name: 'National scale',
-      data: nationalByYear,
-      color: colors.s
-    }]
-  });
-}
+});
