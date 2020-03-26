f22e1d9ab59196142cdfe21e663509cbdf119b84
Bjørn Sandvik <bjorn@mastermaps.com>
2020-03-26 10:36:24 +0100

    Updates

---
+++ precache-manifest.cfb8d8f1f3e0e56ad36195aa4b8ae84c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f89a88d06dbe09ac23824ad33f4f823",
+    "url": "/dhis2-in-action/index.html"
+  },
+  {
+    "revision": "e3c7f0e1a1489248b597",
+    "url": "/dhis2-in-action/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b9d95788ccbb6ba266a3",
+    "url": "/dhis2-in-action/static/css/main.49bcf208.chunk.css"
+  },
+  {
+    "revision": "e3c7f0e1a1489248b597",
+    "url": "/dhis2-in-action/static/js/2.9c104618.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/dhis2-in-action/static/js/2.9c104618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9d95788ccbb6ba266a3",
+    "url": "/dhis2-in-action/static/js/main.cd5644e9.chunk.js"
+  },
+  {
+    "revision": "b02c7b1c94330995b5f3",
+    "url": "/dhis2-in-action/static/js/runtime-main.92469a92.js"
+  }
+]);
