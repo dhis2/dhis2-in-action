03fe73ce5d660119bdea920d9083b5e5988eae76
Bjørn Sandvik <bjorn@mastermaps.com>
2020-04-24 10:30:44 +0200

    Updates

---
+++ precache-manifest.4984b0cbcdb8c20518d09c9625dd794a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acce863eeb64b65834fec81d357efbe2",
+    "url": "/dhis2-in-action/index.html"
+  },
+  {
+    "revision": "2f720ed062f05426af91",
+    "url": "/dhis2-in-action/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "76c052eeaf42f9b8ac24",
+    "url": "/dhis2-in-action/static/css/main.b308fd85.chunk.css"
+  },
+  {
+    "revision": "2f720ed062f05426af91",
+    "url": "/dhis2-in-action/static/js/2.0996c5c1.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/dhis2-in-action/static/js/2.0996c5c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76c052eeaf42f9b8ac24",
+    "url": "/dhis2-in-action/static/js/main.db100c3b.chunk.js"
+  },
+  {
+    "revision": "b02c7b1c94330995b5f3",
+    "url": "/dhis2-in-action/static/js/runtime-main.92469a92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dhis2-in-action/precache-manifest.c25cb64cee602cfb9934ecc72cd414fd.js"
+  "/dhis2-in-action/precache-manifest.4984b0cbcdb8c20518d09c9625dd794a.js"
 );
 
 self.addEventListener('message', (event) => {
