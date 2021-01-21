a5f627b71187cdfc9d4ac3f4857c63e8b2ae55ac
Bjørn Sandvik <bjorn@mastermaps.com>
2021-01-21 10:21:04 +0100

    code changes

--- src/Root.js
+++ src/Root.js
@@ -2,6 +2,9 @@ import React from "react";
 import Fullscreen from "./components/Fullscreen";
 import App from "./App";
 
+// TODO: Read view parameter and toggle between App and AppWho
+// http://localhost:3000/dhis2-in-action?view=who#health
+
 const Root = () => {
   return (
     <Fullscreen>
