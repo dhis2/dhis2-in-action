f4e4f9696192d4c43d656d74412b6cdc7c0f75c6
Bjørn Sandvik <bjorn@mastermaps.com>
2023-06-01 14:59:49 +0200

    Updates

---
+++ precache-manifest.d5df230e5acd3b7a5af849559e407697.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6813efb7bf59a158905123fa063e6f2",
+    "url": "/dhis2-in-action/index.html"
+  },
+  {
+    "revision": "06645ba101ece661e44d",
+    "url": "/dhis2-in-action/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "f6cb0ba3ad4808db5022",
+    "url": "/dhis2-in-action/static/css/main.5f2d0bc1.chunk.css"
+  },
+  {
+    "revision": "06645ba101ece661e44d",
+    "url": "/dhis2-in-action/static/js/2.ec74979b.chunk.js"
+  },
+  {
+    "revision": "2bd18aec346efbf72574573b7442e8aa",
+    "url": "/dhis2-in-action/static/js/2.ec74979b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6cb0ba3ad4808db5022",
+    "url": "/dhis2-in-action/static/js/main.530448f7.chunk.js"
+  },
+  {
+    "revision": "b02c7b1c94330995b5f3",
+    "url": "/dhis2-in-action/static/js/runtime-main.92469a92.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dhis2-in-action/precache-manifest.d24beea724d592314e2a41debd4b7f11.js"
+  "/dhis2-in-action/precache-manifest.d5df230e5acd3b7a5af849559e407697.js"
 );
 
 self.addEventListener('message', (event) => {
